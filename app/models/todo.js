28840ccfe93889cba69a43e7e0851d2d73104d8a
Patrick Werner <pwerner2@gmail.com>
2015-03-01 21:30:30 -0500

    added todos.js

--- app/models/todo.js
+++ app/models/todo.js
@@ -6,19 +6,19 @@ export default DS.Model.extend({
 }).reopenClass({
         FIXTURES: [
             {
-                            id: 1,
-            title: "Complete Ember.js Tutorial",
-            isCompleted: false
+                id: 1,
+                title: "Complete Ember.js Tutorial",
+                isCompleted: false
             },
         {
-                        id: 2,
-            title: "Checkout some more ember stuff",
-            isCompleted: true
+                id: 2,
+                title: "Checkout some more ember stuff",
+                isCompleted: true
             },
         {
-                        id: 3,
-            title: "Solve world hunger (with Ember)",
-            isCompleted: false
+                id: 3,
+                title: "Solve world hunger (with Ember)",
+                isCompleted: false
             }
     ]
 });
